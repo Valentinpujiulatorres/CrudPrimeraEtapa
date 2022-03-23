d4e8bfe9000a34b1f6f8432970bcada567a8dff9
gasparnovel <gnovel@cifpfbmoll.eu>
2022-03-23 12:04:14 +0100

    feat: :bug: Simplify LanguageController

--- CrudPrimeraEstapa/app/Http/Controllers/LanguageController.php
+++ CrudPrimeraEstapa/app/Http/Controllers/LanguageController.php
@@ -5,15 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Redirect;
 
 class LanguageController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function set_language($language){
-        if(array_key_exists($language, config('languages'))){
-            session()->put('applocale', $language);
-        }
-        return back();
+        session()->put('applocale', $language);
+        return Redirect::back();
     }
 }
