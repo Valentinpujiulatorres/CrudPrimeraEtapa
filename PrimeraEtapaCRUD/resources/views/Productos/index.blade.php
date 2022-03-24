804369b39c0b449b24113799b272cfa78ddbbf1d
Valentin <vpujiula@cifpfbmoll.eu>
2022-03-24 15:07:35 +0100

    Index SweetAlert  ScriptTag

--- PrimeraEtapaCRUD/resources/views/Productos/index.blade.php
+++ PrimeraEtapaCRUD/resources/views/Productos/index.blade.php
@@ -5,8 +5,8 @@
 <script>
     function SureToDelete(){
         Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
+  title: 'Estas Seguro?',
+  text: "Una vez echo no podras revertir !",
   icon: 'warning',
   showCancelButton: true,
   confirmButtonColor: '#3085d6',
