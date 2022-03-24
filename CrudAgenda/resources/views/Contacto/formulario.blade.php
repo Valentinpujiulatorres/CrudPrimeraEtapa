92ddb7b1cca93046291ee031a3f47668bf937a9a
david <dgonzalezl@cifpfbmoll.eu>
2022-03-24 16:26:51 +0100

    Subo estilos de la vista index y create que la optengo en formulario.blade.php

--- CrudAgenda/resources/views/Contacto/formulario.blade.php
+++ CrudAgenda/resources/views/Contacto/formulario.blade.php
@@ -1,28 +1,31 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<div>
-    <h1> Empleados </h1>
-    <form>
-    <label for="id">Identificador</label>
-    
-    <label for="NombreContacto">Nombre Contacto </label>
-    <input type="text" name="NombreContacto" id="NombreContacto" placeholder="Escriba su nombre"">
+
+<div class="text-center">
+    <h1 class="text-center p-2" style="color: blue"> Nuevo Contacto  </h1>
+    <a  class="float-end btn btn-dark" href="{{url('contacto/')}}">Retroceder </a>    
+</div>
+
+<div class="text-center">
+    <form class="text-center">
+    <label for="NombreContacto" class="form-label m-2" style="color:rosybrown">Nombre Contacto  </label>
+    <input class="form-text" type="text" name="NombreContacto" id="NombreContacto" placeholder="Escriba su nombre"">
     <br>
-    <label for="Apellidos">Apellidos Contacto</label>
-    <input type="text" name="Apellidos" id="Apellidos" placeholder="Escriba sus apellidos" ">
+    <label for="Apellidos" class="form-label m-2" style="color:rosybrown">Apellidos Contacto</label>
+    <input class="form-text" type="text" name="Apellidos" id="Apellidos" placeholder="Escriba sus apellidos" ">
     <br>
-    <label for="Dirección">Dirección</label>
+    <label for="Dirección" class="form-label m-2" style="color: rosybrown">Dirección</label>
     <input type="text" name="Direccion" id="Direccion" placeholder="Escriba su correo electrónico ">
     <br>
-    <label for="Edad"> Edad </label>
+    <label for="Edad" class="form-label m-2" style="color: rosybrown"> Edad </label>
     <input type="text" name="Edad" id="Edad" placeholder="Que edad tiene">
     <br>
-    <label for="Imagen ">Eliga una imagen </label>
-    <input type="file" name="Imagen" id="Imagen">
+    <label for="Imagen"  class="form-label m-2" style="color:rosybrown">Eliga una imagen </label>
+    <input class="form-text " type="file" name="Imagen" id="Imagen">
     <br>
     <input type="submit" value=" Guardar Contacto">
     <br>
-    <a href="{{url('contacto/')}}">Retroceder 
+    
 </form>
 </div>
--- CrudAgenda/resources/views/Contacto/index.blade.php
+++ CrudAgenda/resources/views/Contacto/index.blade.php
@@ -1,11 +1,12 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<div>
 
-
-    <h1 class="text-center " style="color: blue" > Historico Contactos</h1>
+    <h1 class="text-center p-2 " style="color: blue" > Historico Contactos</h1>
 
 <a class="float-end btn btn-info m-3 " href="{{ url('contacto/create') }}">Crear nuevo Contacto </a>
+</div>
 <table class="table table-dark">
     <thead class="thead-dark">
         <tr>
