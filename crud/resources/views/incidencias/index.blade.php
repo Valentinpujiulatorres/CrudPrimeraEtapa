d5a60212680bd83f66f7e5e07b40089c77630677
Alexgb24 <agonzalez@cifpfbmoll.eu>
2022-03-29 12:35:27 +0200

    Quitar texto del merge

--- crud/resources/views/incidencias/index.blade.php
+++ crud/resources/views/incidencias/index.blade.php
@@ -4,14 +4,11 @@
 
 @section('content')
 
-<<<<<<< HEAD
 <!-- CDN y codigo de la datatable !-->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css"/>
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>    
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>    
-=======
 
->>>>>>> df6c722b89d312dbf4c9f9fe3100efc8a014ab93
 
     
 
